a52d10c6471d65ced9d7c330e2fa6e9f3727ac73
Eman Ib <adla48770@gmail.com>
2020-10-16 19:49:56 +0300

    react claculator

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import react from 'react';
+import React from "react";
+import "./Button.css";
+
+const isOperator = val => {
+  return !isNaN(val) || val === "=" || val === ".";
+};
+
+export const Button = props => (
+  <div
+    className={`Button-wrapper ${
+      isOperator(props.children) ? null:"operator"
+    }`}
+    onClick={() => props.handleClick(props.children)}
+  >
+    {props.children}
+  </div>
+);
+ 
